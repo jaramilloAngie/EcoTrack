0418ca3df878e75aab3640c12d52d68ff7161a56
geralRico <geraldine.florez@correounivalle.edu.co>
2025-04-28 22:10:17 -0500

    correción

--- app/src/main/java/PapelActivity.kt
+++ app/src/main/java/PapelActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ecotrackapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -20,7 +21,7 @@ class PapelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.plastico_activity)
+        setContentView(R.layout.papel_activity)
 
         // Inicializar views
         recyclerView = findViewById(R.id.recyclerViewProductos)
@@ -31,12 +32,14 @@ class PapelActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 2)
 
         val listaProductos = listOf(
-            Producto("Botella Plástica", 100, R.drawable.botella),
-            Producto("Empaque de papas", 30, R.drawable.empaque_papas),
-            Producto("Bolsa plástica", 50, R.drawable.bolsa),
-            Producto("Producto de limpieza", 100, R.drawable.limpieza),
-            Producto("Tapas", 30, R.drawable.tapas),
-            Producto("Vasos", 30, R.drawable.vaso)
+            Producto("Periódico", 100, R.drawable.ic_periodico),
+            Producto("Hojas de papel", 30, R.drawable.ic_hojasdepapel),
+            Producto("Caja de cartón", 100, R.drawable.ic_caja),
+            Producto("Cubeta de huevos", 40, R.drawable.ic_cubetahuevos),
+            Producto("Bolsa de papel", 30, R.drawable.ic_bolsapapel),
+            Producto("vaso de cartón", 30, R.drawable.ic_vasocarton),
+            Producto("Empaque de leche", 30, R.drawable.ic_empaqueleche)
+
         )
 
         adapter = ProductoAdapter(listaProductos) { producto ->
